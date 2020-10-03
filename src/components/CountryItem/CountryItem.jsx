5345b85e68c9917743462fc90e9f82c1ee2f2f64
amaury-diallo <amadoury313@gmail.com>
2020-10-03 21:07:04 +0000

    Style added to CountryItem component

--- src/components/CountryItem/CountryItem.jsx
+++ src/components/CountryItem/CountryItem.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import capitalIcon from './icons/capitale.svg'
 import locationIcon from './icons/location.svg'
 import populationIcon from './icons/population.svg'
+import './country-item.scss'
 
 export default function Countryitem({country}) {
 
@@ -10,22 +11,28 @@ export default function Countryitem({country}) {
         <div className="country-card">
             <h2 className="country-name">{country.name}</h2>
             <div className="country-flag-wrapper">
-                {/* <img src={country.flag} alt={country.name + "'s flag"}/> */}
+                <img src={country.flag} alt={country.name + "'s flag"}/>
             </div>
-            <ul className="details-wrapp">
+            <ul className="details-wrapper">
                 <li className="detail-item">
-                    <img src={capitalIcon} alt="icon"/>
+                    <span className="icon">
+                        <img src={capitalIcon} alt="icon"/>
+                    </span>
                     <span>{country.capital}</span> 
                 </li>
                 
                 <li className="detail-item">
-                    <img src={locationIcon} alt="icon"/>
+                    <span className="icon">
+                        <img src={locationIcon} alt="icon"/>
+                    </span>
                     <span>{country.subregion}</span> 
                 </li>
                 
                 <li className="detail-item">
-                    <img src={populationIcon} alt="icon"/>
-                    <span>{country.population}</span> 
+                    <span className="icon">
+                        <img src={populationIcon} alt="icon"/>
+                    </span>
+                    <span>{String(country.population).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ")}</span> 
                 </li>
             </ul>
         </div>
