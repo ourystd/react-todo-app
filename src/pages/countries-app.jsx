44e0f30e22eee494ebd78e3e35e7e7d2993b6bda
amaury-diallo <amadoury313@gmail.com>
2020-10-04 06:08:14 +0000

    Refactor: text correction

--- src/pages/countries-app.jsx
+++ src/pages/countries-app.jsx
@@ -4,7 +4,7 @@ import Countireslist from "../components/CountiresList/CountiresList";
 export default function CountriesApp() {
 	return (
 		<div>
-			<h1 className="text-center py-2">African Counties</h1>
+			<h1 className="text-center py-2">African Countries</h1>
 			<Countireslist />
 		</div>
 	);
