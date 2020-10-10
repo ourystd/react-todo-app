18b08f3ef2a09328304751ece56d29174771438a
amaury-diallo <amadoury313@gmail.com>
2020-10-10 11:14:49 +0000

    Playing with hooks

--- src/components/CountiresList/CountiresList.jsx
+++ src/components/CountiresList/CountiresList.jsx
@@ -1,34 +1,35 @@
-import React, { Component } from 'react'
-import Countryitem from '../CountryItem/CountryItem'
+import React, { Component } from "react";
+import Countryitem from "../CountryItem/CountryItem";
 
-import axios from 'axios'
+import axios from "axios";
 
 export default class Countireslist extends Component {
-    
-    state = {
-        countries: [],
-        loading: true,
-    }
-    
-    componentDidMount() {
-        axios.get(`https://restcountries.eu/rest/v2/region/africa`)
-        .then(response => {
-            this.setState({countries: response.data, loading: false});
-        })
-    }
-    
-    
+  state = {
+    countries: [],
+    loading: true,
+  };
 
-    render() {
-        return (
-            (this.state.loading) ? 'Fecthing data...'  :
-            <div className="row countries-list">
-            {
-                this.state.countries.map((country, index) => (
-                    <Countryitem key={index} country={country} />
-                ))
-            }
-            </div>
-        )
-    }
+  componentDidMount() {
+    axios
+      .get(`https://restcountries.eu/rest/v2/region/africa`)
+      .then((response) => {
+        this.setState({ countries: response.data, loading: false });
+      });
+  }
+
+  render() {
+    return this.state.loading ? (
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border text-secondary" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    ) : (
+      <div className="row countries-list">
+        {this.state.countries.map((country, index) => (
+          <Countryitem key={index} country={country} />
+        ))}
+      </div>
+    );
+  }
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,45 +2,50 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-	return (
-		<div>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<NavLink to="/" className="navbar-brand" style={{ padding: 10 }}>
-					React Projects
-				</NavLink>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarNav"
-					aria-controls="navbarNav"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav">
-						<li className="nav-item">
-							<NavLink exact={true} to="/" className="nav-link">
-								All
-							</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink to="/todo-app" className="nav-link">
-								Todo App
-							</NavLink>
-						</li>
-						<li className="nav-item">
-							<NavLink to="/countries-app" className="nav-link">
-								Countries App
-							</NavLink>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-	);
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink to="/" className="navbar-brand" style={{ padding: 10 }}>
+          React Projects
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink exact={true} to="/" className="nav-link">
+                All
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/todo-app" className="nav-link">
+                Todo App
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/countries-app" className="nav-link">
+                Countries App
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/todo-hooks" className="nav-link">
+                Todo Hooks
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
