d2daa8a9d4ee0a880603dbbcb8ba149e22875f51
amaury-diallo <amadoury313@gmail.com>
2020-10-10 16:47:18 +0000

    Refactoring: 'App' from class to functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.scss";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -8,22 +8,18 @@ import TodoApp from "./pages/todo-app";
 import CountriesApp from "./pages/countries-app";
 import NavBar from "./components/Navbar/Navbar";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="container">
-          <NavBar />
-          <Switch>
-            <Route path="/todo-app" exact component={TodoApp} />
-            <Route path="/countries-app" exact component={CountriesApp} />
-            <Route path="/" exact component={Home} />
-            <Route component={NotFound404} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <NavBar />
+      <Switch>
+        <Route path="/todo-app" exact component={TodoApp} />
+        <Route path="/countries-app" exact component={CountriesApp} />
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound404} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
