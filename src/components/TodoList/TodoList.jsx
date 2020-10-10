e8f0306ed1a9aac8eacbde7035608e1bcc39e5c1
amaury-diallo <amadoury313@gmail.com>
2020-10-10 16:06:27 +0000

    Refactoring TodoList: from class to funtionnal component with hooks

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,122 +1,117 @@
-import React, {Component} from 'react'
+import React, { useState } from "react";
 
-import './todolist.scss'
+import "./todolist.scss";
 
-import { ListGroup } from 'reactstrap'
-import { Alert } from 'reactstrap';
+import { ListGroup } from "reactstrap";
+import { Alert } from "reactstrap";
 
-import TodoItem from '../TodoItem/TodoItem'
-import NewTaskForm from '../NewTaskForm/NewTaskForm'
+import TodoItem from "../TodoItem/TodoItem";
+import NewTaskForm from "../NewTaskForm/NewTaskForm";
 
+const TodoList = () => {
+  const [state, setState] = useState({
+    tasks: [
+      { id: 1, name: "Task 1", done: false },
+      { id: 2, name: "Task 2", done: false },
+      { id: 3, name: "Task 3", done: true },
+      { id: 4, name: "Task 4", done: false },
+      { id: 5, name: "Task 5", done: true },
+    ],
+    newTaskName: "",
+  });
 
-class TodoList extends Component{
+  const createTask = (e) => {
+    e.preventDefault();
 
-	state = {
-		tasks: [
-			{id: 1, name: 'Task 1', done: false},
-			{id: 2, name: 'Task 2', done: false},
-			{id: 3, name: 'Task 3', done: true},
-			{id: 4, name: 'Task 4', done: false},
-			{id: 5, name: 'Task 5', done: true},
-		],
-		newTaskName: '' 
-	}
+    const newTask = {
+      id: state.tasks.length + 1,
+      name: state.newTaskName,
+      done: false,
+    };
+    console.log(newTask);
+    setState((prevState) => ({
+      tasks: [...prevState.tasks, newTask],
+      newTaskName: "",
+    }));
+  };
 
-	
+  const deteleTask = (id) => {
+    const indexToDelete = state.tasks.findIndex((task) => task.id === id);
+    let newTasks = [...state.tasks];
+    newTasks.splice(indexToDelete, 1);
 
-	createTask = (e) => {
-		e.preventDefault()
+    setState({
+      tasks: newTasks,
+    });
 
-		const newTask = {
-			id: this.state.tasks.length + 1,
-			name: this.state.newTaskName,
-			done: false
-		}
-		console.log(newTask)
-		this.setState( prevState => ({ 
-				tasks: [...prevState.tasks, newTask],
-				newTaskName: ''
-			})
-		)
-	}
+    console.log(newTasks);
+  };
 
-	deteleTask =  (id) => {
-		const indexToDelete = this.state.tasks.findIndex(task => task.id === id)
-		let newTasks = [...this.state.tasks]
-		newTasks.splice(indexToDelete, 1)
+  const completeTask = (id) => {
+    // task.done = true
+    const newTasks = state.tasks.map((task) => {
+      if (task.id === id) {
+        task.done = task.done ? false : true;
+      }
 
-		this.setState({
-			tasks: newTasks
-		})
+      return task;
+    });
 
-		console.log(newTasks)
-	}
+    setState({
+      tasks: newTasks,
+    });
 
-	completeTask =  (id) => {
-		// task.done = true
-		const newTasks = this.state.tasks.map(task => {
-			if (task.id === id) {
-				task.done = (task.done) ? false: true;
-			}
+    console.log(state.tasks);
+    // css effect
+  };
 
-			return task
-		})
+  const handleChange = (e) => {
+    const newTaskName = e.currentTarget.value;
+    setState((prevState) => ({
+      ...prevState,
+      newTaskName,
+    }));
+  };
 
-		this.setState({
-			tasks: newTasks
-		})
+  const editTask = (id, event) => {
+    const newTasks = state.tasks.map((task) => {
+      if (task.id === id) {
+        task.name = event.target.value;
+      }
 
-		console.log(this.state.tasks)
-		// css effect
-	}
+      return task;
+    });
 
-	handleChange = (e) => {
-		this.setState({
-			newTaskName: e.target.value
-		})
-	}
+    setState({
+      tasks: newTasks,
+    });
+  };
+  return (
+    <div>
+      <ListGroup>
+        {state.tasks.map((task) => (
+          <TodoItem
+            key={task.id}
+            task={task}
+            onDelete={deteleTask}
+            onStatusChange={completeTask}
+            editTask={editTask}
+          />
+        ))}
+      </ListGroup>
 
-	editTask = (id, event) => {
-		const newTasks = this.state.tasks.map(task => {
-			if (task.id === id) {
-				task.name = event.target.value
-			}
+      {!state.tasks.length && (
+        <Alert color="light">
+          There are no tasks available. You can create one.
+        </Alert>
+      )}
+      <NewTaskForm
+        currentValue={state.newTaskName}
+        handleChange={handleChange}
+        createTask={createTask}
+      />
+    </div>
+  );
+};
 
-			return task
-		})
-
-		this.setState({
-			tasks: newTasks
-		})
-	}
-
-	render(){
-
-		return(
-			<div>
-				<ListGroup>
-				{
-					this.state.tasks.map(task => (
-						<TodoItem 
-							key={task.id} 
-							task={task}
-							onDelete={this.deteleTask}
-							onStatusChange={this.completeTask}
-							editTask={this.editTask}
-						/>
-					)) 
-				}
-				</ListGroup>
-
-				{!this.state.tasks.length && <Alert color="light">There are no tasks available. You can create one.</Alert> }
-				<NewTaskForm 
-					currentValue={this.state.newTaskName}
-					handleChange={this.handleChange}
-					createTask={this.createTask}
-				/>
-			</div>
-		)
-	}
-}
-
-export default TodoList
+export default TodoList;
